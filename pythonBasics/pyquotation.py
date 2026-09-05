79eb5c634d826ac7bf2e3b637eaa54d7c6019ca1
jobiabo <joelobiabo@gmail.com>
2026-09-05 22:13:01 +0100

    added: python basics-string quotes

---
+++ pythonBasics/pyquotation.py
@@ -0,0 +1,12 @@
+word = 'word'
+print (word)
+
+sentence = "This is a sentence."
+print (sentence)
+
+
+#this block supports multi-line paragraph and spacing
+paragraph = """This is a paragraph. It is
+ made up of multiple lines and 
+                      sentences."""
+print (paragraph)
