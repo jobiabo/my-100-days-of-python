ada974b96bab1f4ceacb6423f0f269abc64ade29
jobiabo <joelobiabo@gmail.com>
2026-09-15 16:02:57 +0100

    added python-count-list

---
+++ pythonBasics/datatype.py
@@ -0,0 +1,16 @@
+# datatype ==> python LIST
+x = ["james", "arnold", "david", "dawud"]
+print(x, type(x))
+
+# datatype ==> python SETS
+
+y = {"water", "sound", "air", "fire", "land"}
+print(y, type(y))
+
+# datatype ==> python TUPLE
+z = ("hello","bye", "see you")
+print(z, type(z))
+
+# datatype ==> python DICT
+ai = {"name": "john", "class": "200lv"}
+print(ai, type(ai))
---
+++ pythonBasics/pythoncountlist.py
@@ -0,0 +1,5 @@
+numbers = [1, 2, 3, 4, 5]
+
+count = len(numbers)
+if count > 3:
+    print(f"list has {count} numbers")
