5a595ff6ae1ed4df3d8d27e8f02993781f2f065e
jobiabo <joelobiabo@gmail.com>
2026-09-24 09:43:15 +0100

    fix: more python-dict-loop-access

--- pythonBasics/pythondict.py
+++ pythonBasics/pythondict.py
@@ -4,3 +4,23 @@ data = {
     "sex": "male"
 }
 print(data)
+print("\n")
+
+
+# for x in data:
+#     print(data[x])
+
+#looping through dict using the key() to access the key
+for key in data.keys():
+    print(key)
+    
+print("\n")
+#looping through dict using the values() to access the values
+for value in data.values():
+    print(value)
+print("\n")
+
+#looping through dict using the item() to print both key and vale
+
+for x, y in data.items():
+    print(x, y)
